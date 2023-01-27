07a762a3f63d67880f75b3c1227040cc9861fa79
Michael Xiang <14zx4@queensu.ca>
2023-01-27 10:46:32 -0500

    final

--- src/components/Rundown.js
+++ src/components/Rundown.js
@@ -40,7 +40,7 @@ const Rundown = () => {
               <tr>
                 <td>7:00pm - 7:25pm</td>
                 <td>Opening, Johnson and Renee speech, lion dance, Red Pocket</td>
-                <td>Michael,Jackie</td>
+                <td>Michael,Jackie, Vitus</td>
                 <td>PPT for presentation / music for Red pocket </td>
               </tr>
               <tr>
@@ -53,43 +53,37 @@ const Rundown = () => {
                 <td>7:30pm - 7:45pm</td>
                 <td>Charity promotion/presentation: Sickkids & Carefirst & Markham Food Bank</td>
                 <td>Guests</td>
-                <td>Mayor singing USB/ presentation PPT</td>
+                <td>PPT for presentation</td>
               </tr>
               <tr>
                 <td>7:45pm - 8:00pm</td>
                 <td>Lucky draw-1st run</td>
-                <td>Luck draw team</td>
+                <td>Lucky draw team- Yuki, Vanessa, Simran, Helen Li</td>
                 <td>New year songs</td>
               </tr>
               <tr>
                 <td>8:00pm - 8:10pm</td>
                 <td>singing performance  2 songs</td>
-                <td>Hugh Wang 1.朋友 2. You raise me up</td>
+                <td>爱乐乐团 1. 好运来（二重唱） 2.举杯吧朋友 （四重唱）</td>
                 <td>music-USB</td>
               </tr>
               <tr>
-                <td>8:10pm - 8:20pm</td>
+                <td>8:10pm - 8:30pm</td>
                 <td>Lucky draw-2nd run </td>
                 <td>Lucky draw team- Kristine, Shirley, Jenny, Lucy</td>
                 <td>New year songs</td>
               </tr>
               <tr>
-                <td>8:20pm - 8:30pm</td>
-                <td>singing performance  2 songs</td>
-                <td>爱乐乐团 1. 举杯吧朋友 2.好运来</td>
-                <td>music-USB</td>
-              </tr>
-              <tr>
                 <td>8:30pm - 8:45pm</td>
-                <td>Markham Mayor Greeting/Singing/photos</td>
-                <td>Mayor and guests</td>
-                <td></td>
+                <td>Markham Mayor Greeting/Singing/ photos</td>
+                <td>Mayor and guests </td>
+                <td>Mayor singing music-USB</td>
               </tr>
               <tr>
                 <td>8:45pm - 9:00pm</td>
                 <td>Dance performance</td>
                 <td>3 professionsal dancers 1. Angels 2. bellydance 3. Samba</td>
-                <td></td>
+                <td>Music </td>
               </tr>
               <tr>
                 <td>9:00pm - 9:15pm</td>
@@ -101,7 +95,7 @@ const Rundown = () => {
                 <td>9:15pm - 9:20pm</td>
                 <td>JF Staff singing (1 song)</td>
                 <td>All staff</td>
-                <td></td>
+                <td>music-USB</td>
               </tr>
               <tr>
                 <td>9:20pm- 9:30pm</td>
